0561f3fc43a2b43ccc221a88e0b647438fac07a6
Ukahodariya <drashtikahodariya64036@gmail.com>
2024-09-02 11:42:40 +0530

    02-09-2024

--- src/categoryApi/categoryApi.js
+++ src/categoryApi/categoryApi.js
@@ -1,11 +1,56 @@
 import { FaChevronDown } from "react-icons/fa";
+import Imgone from '../assets/image/medicine-one.webp';
+import Imgtwo from '../assets/image/medicine-two.webp';
+import Imgthree from '../assets/image/medicine-three.webp';
 
 const categoryApi = [
     {
         category: 'Medicine',
         path: 'Medicine',
         micon: FaChevronDown,
-        product: []
+        medicineDetails : [
+            {
+                title: "Sri Sri Tattva Mehantaka Vati 1000mg Tablet | For Blood Sugar Control",
+                image: Imgone,
+                quantity: "bottle of 60 tablets",
+                price: 400,
+                mrp: 500,
+                discount: "20% OFF"
+            },
+            {
+                title: "Pintola Classic Peanut Butter American Recipe Crunchy",
+                image: Imgtwo,
+                quantity: "bottle of 60 tablets",
+                price: 400,
+                mrp: 500,
+                discount: "20% OFF"
+            },
+            {
+                title: "Macprot DM with CoQ10, Vitamins & Minerals for Diabetics | Flavour Powder Vanilla",
+                image: Imgthree,
+                quantity: "bottle of 60 tablets",
+                price: 400,
+                mrp: 500,
+                discount: "20% OFF"
+            },
+            {
+                title: "Dr Morepen Gluco One BG 03 Blood Glucose Test Strip (Only Strips) | Diabetes Monitoring",
+                image: Imgtwo,
+                quantity: "bottle of 60 tablets",
+                price: 400,
+                mrp: 500,
+                discount: "20% OFF"
+            },
+            {
+                title: "OneTouch Select Plus Test Strip (Only Strips) Test Strip Green",
+                image: Imgone,
+                quantity: "bottle of 60 tablets",
+                price: 400,
+                mrp: 500,
+                discount: "20% OFF"
+            }
+        ]
+        
     },
     {
         category: 'Healthcare',
--- src/components/medicine/index.jsx
+++ src/components/medicine/index.jsx
@@ -1,19 +1,51 @@
 import React from 'react'
 import './medicine.scss'
 import { IoSearchSharp } from "react-icons/io5";
+import categoryApi from '../../categoryApi/categoryApi';
 
 export default function Medicine() {
+    const data = categoryApi[0].medicineDetails;
+    console.log("data", data)
     return (
         <div className='medicine'>
             <div className='container'>
                 <div className='medicine-search'>
-                    <input type="text" placeholder='Search Medicines' />  
-                    <IoSearchSharp className='search-icon'/>
+                    <input type="text" placeholder='Search Medicines' />
+                    <IoSearchSharp className='search-icon' />
                 </div>
-                <div>
-
+                <div className='medicine-content'>
+                    <div className='medicine-card-diabetes'>
+                        <div className='medicine-card-heading'>
+                            <h2>Diabetes</h2>
+                        </div>
+                        <div className='medicine-card-grid'>
+                            {data.map((item) => (
+                                <div className='medicine-card-description'>
+                                    <div className='medicine-card-description-image'>
+                                        <img src={item.image}></img>
+                                    </div>
+                                    <div className='medicine-card-description-text-alignment'>
+                                        <div className='medicine-card-description-text'>
+                                            <h3>{item.title}</h3>
+                                            <span>{item.quantity}</span>
+                                            <div className='medicine-card-description-flex'>
+                                                <a>₹{item.price}</a>
+                                                <p>MRP <del>₹{item.mrp}</del></p>
+                                                <span>{item.discount}</span>
+                                            </div>
+                                            <div className='medicine-card-description-btn'>
+                                                <button>Add To Cart</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
+
